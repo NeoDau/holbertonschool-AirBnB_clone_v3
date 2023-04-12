e6cb68f9bd5e5607dd199527711c3d7aa90ca8b6
NeoDau <neodau@gmail.com>
2023-04-12 16:48:52 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,24 +7,22 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
 def close_storage(exception):
-    """Comments"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Comments"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
-    """Comments"""
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', '5000'))
 
